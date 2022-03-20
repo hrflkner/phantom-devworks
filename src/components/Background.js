309d5af1be7a1cb0d7e137a9be72a53bba9fa43d
hrflkner <hunter.wm.faulkner@gmail.com>
2022-03-20 14:19:34 +1100

    Initialize Components, Images, SCSS Styles

---
+++ src/components/Background.js
@@ -0,0 +1,55 @@
+import {
+    ScrollContainer,
+    ScrollPage,
+    Animator,
+    FadeIn,
+    MoveIn,
+    batch,
+} from 'react-scroll-motion';
+import mainBackgroundOne from '../images/phantom-design-works-main.png';
+//import ghost3 from '../images/body-ghost-1.png';
+import mainBackgroundTwo from '../images/phantom-dev-works-background-2.png';
+
+function Background() {
+    return (
+        <ScrollContainer>
+            <ScrollPage page={1}>
+                <Animator animation={batch(FadeIn(), MoveIn(0, 10))}>
+                    <img
+                        className="main-background-1 slideInUp"
+                        src={mainBackgroundOne}
+                        alt="Main Background 1"
+                    />
+                    <img
+                        className="main-background-2"
+                        src={mainBackgroundTwo}
+                        alt="Main Background 2"
+                    />
+                </Animator>
+            </ScrollPage>
+        </ScrollContainer>
+        // <>
+        //     <section>
+        //         <ScrollContainer>
+        //             <ScrollPage>
+        //                 <Animator animation={batch(FadeIn(), MoveIn(0, -200))}>
+        //                     <img
+        //                         className="main-background-1 slideInUp"
+        //                         src={mainBackgroundOne}
+        //                         alt="Main Background 1"
+        //                     />
+        //                 </Animator>
+        //             </ScrollPage>
+        //         </ScrollContainer>
+        //         <img className="ghost-three" src={ghost3} alt="Ghost 3" />
+        //         <img
+        //             className="main-background-2"
+        //             src={mainBackgroundTwo}
+        //             alt="Main Background 2"
+        //         />
+        //     </section>
+        // </>
+    );
+}
+
+export default Background;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <section className="logo-block fadeIn">
+                    <h1 className="logo">phantom devWorks</h1>
+                    <span>by Hunter Faulkner</span>
+                </section>
+            </header>
+        </>
+    );
+}
+
+export default Header;
