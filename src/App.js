d022f54cdbf51e5ab0473c553b3ec6069877ce44
hrflkner <hunter.wm.faulkner@gmail.com>
2022-03-20 14:18:58 +1100

    Initialize and Delete Unnecessary Imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Background from './components/Background';
+import './css/globals.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <section className="blurb">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Dolorum voluptas et quasi nam laboriosam illum soluta ab
+                    placeat sint beatae iure sequi, quidem voluptates, alias
+                    officia vel saepe at dolores molestias possimus quis
+                    dignissimos! Officia, quasi sapiente. In corrupti similique
+                    labore perferendis quas eaque molestias voluptates
+                    blanditiis! Velit mollitia deserunt iure, voluptates
+                    voluptate harum laboriosam voluptatem consequuntur neque
+                    excepturi illo. Quisquam odio laborum ratione molestias enim
+                    distinctio tenetur, quis aperiam amet, iure maiores
+                    perspiciatis, recusandae consectetur perferendis delectus et
+                    repellat velit consequatur accusantium in! Dolor error eius
+                    minus repellendus maiores! Quisquam recusandae, quasi
+                    corporis asperiores animi quo quod autem obcaecati.
+                </p>
+            </section>
+            <Background />
+        </div>
+    );
 }
 
 export default App;
