247fae215d327353a3d18ca6b89dba0f78a1328b
hrflkner <hunter.wm.faulkner@gmail.com>
2022-03-21 17:17:42 +1100

    Update App Imports and Components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import Header from './components/Header';
 import Background from './components/Background';
+import Header from './components/Header';
+import Headline from './components/Headline';
 import './css/globals.css';
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <section className="blurb">
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Dolorum voluptas et quasi nam laboriosam illum soluta ab
-                    placeat sint beatae iure sequi, quidem voluptates, alias
-                    officia vel saepe at dolores molestias possimus quis
-                    dignissimos! Officia, quasi sapiente. In corrupti similique
-                    labore perferendis quas eaque molestias voluptates
-                    blanditiis! Velit mollitia deserunt iure, voluptates
-                    voluptate harum laboriosam voluptatem consequuntur neque
-                    excepturi illo. Quisquam odio laborum ratione molestias enim
-                    distinctio tenetur, quis aperiam amet, iure maiores
-                    perspiciatis, recusandae consectetur perferendis delectus et
-                    repellat velit consequatur accusantium in! Dolor error eius
-                    minus repellendus maiores! Quisquam recusandae, quasi
-                    corporis asperiores animi quo quod autem obcaecati.
-                </p>
-            </section>
+            <Headline />
             <Background />
         </div>
     );
