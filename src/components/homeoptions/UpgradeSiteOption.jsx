861a9e5853c57e64b3bf97db181553fcce6ae0c4
hrflkner <hunter.wm.faulkner@gmail.com>
2022-10-02 19:51:58 +1100

    Fix Definition List Width

--- src/components/homeoptions/UpgradeSiteOption.jsx
+++ src/components/homeoptions/UpgradeSiteOption.jsx
@@ -30,58 +30,60 @@ function UpgradeSiteOption() {
                             alt=""
                         />
                     </section>
-                    <h4>Today's Essentials:</h4>
-                    <dl>
-                        <dt className="fadeInRight">Responsiveness</dt>
-                        <dd className="fadeInLeft">
-                            Designs that look great on{' '}
-                            <span className="highlight">all devices</span>!
-                            Users on{' '}
-                            <span className="highlight">
-                                mobile, tablet, or desktop
-                            </span>{' '}
-                            should have no trouble interacting with your site to
-                            get to know your business.
-                        </dd>
-                        <img
-                            src={ResponsiveIcon}
-                            alt="Responsiveness Icon"
-                            className="deflist-icons fadeInLeft"
-                        ></img>
-                        <dt className="fadeInRight">Accessibility</dt>
-                        <dd className="fadeInLeft">
-                            Offer user friendly experiences that are considerate
-                            of those{' '}
-                            <span className="highlight">
-                                requiring special accomodations
-                            </span>{' '}
-                            such as screenreaders.
-                        </dd>
-                        <img
-                            src={AccessibleIcon}
-                            alt="Accessible Icon"
-                            className="deflist-icons fadeInLeft"
-                        ></img>
-                        <dt className="fadeInRight">Discoverability</dt>
-                        <dd className="fadeInLeft">
-                            Engineering your site to be{' '}
-                            <span className="highlight">
-                                shareable on social media
-                            </span>{' '}
-                            and{' '}
-                            <span className="highlight">
-                                discoverable in search engines
-                            </span>{' '}
-                            contributes to higher visibility online. This helps
-                            to allow more people to find your service or product
-                            at the top of the search results!
-                        </dd>
-                        <img
-                            src={SearchableIcon}
-                            alt="Searchable Icon"
-                            className="deflist-icons fadeInLeft"
-                        ></img>
-                    </dl>
+                    <section className="upgrade-secondary">
+                        <h4>Today's Essentials:</h4>
+                        <dl>
+                            <dt className="fadeInRight">Responsiveness</dt>
+                            <dd className="fadeInLeft">
+                                Designs that look great on{' '}
+                                <span className="highlight">all devices</span>!
+                                Users on{' '}
+                                <span className="highlight">
+                                    mobile, tablet, or desktop
+                                </span>{' '}
+                                should have no trouble interacting with your
+                                site to get to know your business.
+                            </dd>
+                            <img
+                                src={ResponsiveIcon}
+                                alt="Responsiveness Icon"
+                                className="deflist-icons fadeInLeft"
+                            ></img>
+                            <dt className="fadeInRight">Accessibility</dt>
+                            <dd className="fadeInLeft">
+                                Offer user friendly experiences that are
+                                considerate of those{' '}
+                                <span className="highlight">
+                                    requiring special accomodations
+                                </span>{' '}
+                                such as screenreaders.
+                            </dd>
+                            <img
+                                src={AccessibleIcon}
+                                alt="Accessible Icon"
+                                className="deflist-icons fadeInLeft"
+                            ></img>
+                            <dt className="fadeInRight">Discoverability</dt>
+                            <dd className="fadeInLeft">
+                                Engineering your site to be{' '}
+                                <span className="highlight">
+                                    shareable on social media
+                                </span>{' '}
+                                and{' '}
+                                <span className="highlight">
+                                    discoverable in search engines
+                                </span>{' '}
+                                contributes to higher visibility online. This
+                                helps to allow more people to find your service
+                                or product at the top of the search results!
+                            </dd>
+                            <img
+                                src={SearchableIcon}
+                                alt="Searchable Icon"
+                                className="deflist-icons fadeInLeft"
+                            ></img>
+                        </dl>
+                    </section>
                 </section>
             </section>
             <Background>
